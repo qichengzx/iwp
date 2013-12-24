5b8f5d4b618e1a2fb27f12b5541d15eb2109b26c
qichengzx <qichengzx@gmail.com>
2013-12-24 09:35:57 +0800

    loop修改
    添加判断列表页显示密码保护的文章的提示

--- wp-content/themes/wp/loop.php
+++ wp-content/themes/wp/loop.php
@@ -9,7 +9,11 @@
 		<div class="info"><?php entry_author(); ?><span><a href="<?php the_permalink(); ?>#comments"><?php comments_number('0', '1', '%' );?>条评论</a></span><?php entry_date(); ?><?php edit_post_link( __( '编辑', '' ), '<span class="edit-link">', '</span>' ); ?></div>
 	</header>
 	<div class="entry-summary">
+		<?php if ( post_password_required() ): ?>
+			<p>这是一篇受密码保护的文章，您需要提供访问密码</p>
+		<?php else: ?>
 		<?php echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0,300,"...","utf-8"); ?>
+		<?php endif;?>
 	</div><!-- .entry-summary -->
 </article>
 		
