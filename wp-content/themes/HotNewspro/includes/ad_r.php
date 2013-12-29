1e3dfa377bb2f45f6252f789007ae354ffb197d5
zhaoxin <qichengzx@gmail.com>
2013-12-29 20:28:13 +0800

    删除除主题外无用文件

--- wp-content/themes/HotNewspro/includes/ad_r.php
+++
@@ -1,3 +0,0 @@
-<div class="ad_r">
-	<?php echo stripslashes(get_option('swt_ad_rc')); ?>
-</div>
--- wp-content/themes/vantage/extras/settings/message.php
+++
@@ -1,3 +0,0 @@
-<?php
-$theme = basename( get_template_directory() );
-printf( __("I hope %s is working well for you! Sign up to <a href='%s' target='_blank'>our newsletter</a> if you'd like more free themes and goodies.", 'vantage'), ucfirst($theme), 'http://eepurl.com/hbvhM');
